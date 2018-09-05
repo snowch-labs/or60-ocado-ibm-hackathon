5114853e1443df27e27906f0c4c50b4e7c0425ee
Chris Snow <chsnow123@gmail.com>
2018-09-05 16:15:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data_loader',
+      version='0.1',
+      description='Hackathon data loader',
+      url='https://github.com/snowch-labs/or60-ocado-ibm-hackathon',
+      author='Chris Snow',
+      author_email='chris.snow@uk.ibm.com',
+      license='Apache 2.0',
+      packages=['data_loader'],
+      zip_safe=False)
