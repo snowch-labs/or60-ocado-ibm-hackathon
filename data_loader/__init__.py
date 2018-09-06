6b62dad46476ea7ca82df8e3e0b6ad0db23ab55d
Chris Snow <chsnow123@gmail.com>
2018-09-06 09:50:47 +0100

    Update __init__.py

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -9,21 +9,20 @@ class DataLoader:
     
   def __init__(this, credentials):
     this.credentials = credentials
- 
     this.client = ibm_boto3.client(service_name='s3',
         ibm_api_key_id=this.credentials['IBM_API_KEY_ID'],
         ibm_auth_endpoint=this.credentials['IBM_AUTH_ENDPOINT'],
         config=Config(signature_version='oauth'),
         endpoint_url=this.credentials['ENDPOINT'])
-
-  def test_function(this):
-    print(this.credentials)
-
-  def load_order_details_1000_pkl(this):
+    
+  def _load_pickle(this, filename):
     def __iter__(self): return 0
     body = this.client.get_object(
         Bucket=this.credentials['BUCKET'],
-        Key='order_details_1000.pkl'
+        Key=filename
         )['Body']
     if not hasattr(body, "__iter__"): body.__iter__ = types.MethodType( __iter__, body )
     return pickle.loads(body.read())
+
+  def load_order_details_1000(this):
+    return _load_pickle('order_details_1000.pkl')
